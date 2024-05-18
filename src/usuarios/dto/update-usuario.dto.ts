98577317246b334a65e81b2eedfe5f80009372e4
Vanessa <nessa1vane@icloud.com>
2024-05-18 11:06:24 -0300

    feat: finalizando crud

--- src/usuarios/dto/update-usuario.dto.ts
+++ src/usuarios/dto/update-usuario.dto.ts
@@ -1,4 +1,17 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUsuarioDto } from './create-usuario.dto';
+import { IsEmail, IsNotEmpty, IsOptional, MinLength } from 'class-validator';
+import { IsEmailUnico } from './email.validator';
 
-export class UpdateUsuarioDto extends PartialType(CreateUsuarioDto) {}
+export class UpdateUsuarioDTO {
+  @IsNotEmpty({ message: 'O nome não pode ser vazio' })
+  @IsOptional()
+  nome: string;
+
+  @IsEmail(undefined, { message: 'O e-mail informado é inválido' })
+  @IsEmailUnico({ message: 'Já existe um usuário com este e-mail' })
+  @IsOptional()
+  email: string;
+
+  @MinLength(6, { message: 'A senha precisa ter pelo menos 6 caracteres' })
+  @IsOptional()
+  senha: string;
+}
